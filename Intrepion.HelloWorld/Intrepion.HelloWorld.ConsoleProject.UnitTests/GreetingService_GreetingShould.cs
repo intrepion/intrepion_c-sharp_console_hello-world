8ff03388a6870d4f918027e3a8f591a4081a9f62
Oliver Forral <intrepion@gmail.com>
2025-01-08 21:56:45 -0700

    Added second test for Bob.

--- Intrepion.HelloWorld/Intrepion.HelloWorld.ConsoleProject.UnitTests/GreetingService_GreetingShould.cs
+++ Intrepion.HelloWorld/Intrepion.HelloWorld.ConsoleProject.UnitTests/GreetingService_GreetingShould.cs
@@ -30,4 +30,29 @@ public class GreetingService_GreetingShould
         // Assert
         await Assert.That(lines[1]).IsEqualTo(expected);
     }
+
+    [Test]
+    [NotInParallel(ConsoleTest)]
+    [Arguments("Bob")]
+    [Arguments("     Bob       ")]
+    [Arguments("  Bob \n ")]
+    [Arguments("    Bob \r  ")]
+    [Arguments("\t\t\tBob\t\t\t")]
+    public async Task BeHelloBob_GivenBob(string bob)
+    {
+        // Arrange
+        var expected = "Hello, Bob!";
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var textReader = new StringReader(bob);
+        Console.SetIn(textReader);
+
+        // Act
+        Greeter.GetName();
+        var stringBuilder = writer.GetStringBuilder();
+        var lines = stringBuilder.ToString().Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+
+        // Assert
+        await Assert.That(lines[1]).IsEqualTo(expected);
+    }
 }
